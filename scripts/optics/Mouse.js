1d21fdac161219119b4ece57b8190781017cb528
Kantack, Nicholas R <nick.kantack@jhuapl.edu>
2021-10-04 16:41:42 -0400

    Revert "Trying to get touch working for optics simulator."
    This reverts commit a372abdc97a2b24e0aebf7e3b19a03d3b0d22ff3.

--- scripts/optics/Mouse.js
+++ scripts/optics/Mouse.js
@@ -8,10 +8,8 @@ let executeMouseDown = function(e) {
 		mouseIsDown = true;
 	}
 	let rect = canvas.getBoundingClientRect();
-	let x = e.clientX || e.targetTouches[0].pageX;
-	let y = e.clientY || e.targetTouches[0].pageY;
-	mousePos.x = x - rect.left;
-	mousePos.y = y - rect.top;
+	mousePos.x = e.clientX - rect.left;
+	mousePos.y = e.clientY - rect.top;
 	// Select an object if necessary
 	for (opticIndex in optics){
 		if (mousePos.distanceTo(optics[opticIndex].pos) < optics[opticIndex].radius){
@@ -44,9 +42,7 @@ canvas.addEventListener('touchend', executeMouseUp);
 let executeMouseMove = function(e) {
 	if (mouseIsDown){
 		let rect = canvas.getBoundingClientRect();
-		let x = e.clientX || e.targetTouches[0].pageX;
-		let y = e.clientY || e.targetTouches[0].pageY;
-		let newMousePosition = VectorFactory.create(x - rect.left, y - rect.top);
+		let newMousePosition = VectorFactory.create(e.clientX - rect.left, e.clientY - rect.top);
 		let changeVector = newMousePosition.sub(mousePos);
 		mousePos = newMousePosition;
 		// If an object is selected, move its pos by the changeVector
