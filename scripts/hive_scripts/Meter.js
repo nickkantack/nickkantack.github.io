a92915c0e2789dd4fc1bf793d113a0c467959e16
Kantack, Nicholas R <nick.kantack@jhuapl.edu>
2021-10-17 07:53:06 -0400

    Added in precaution to try to avoid meter bars popping out

--- scripts/hive_scripts/Meter.js
+++ scripts/hive_scripts/Meter.js
@@ -10,9 +10,18 @@ function updateProb(probability) {
     if (probability !== 0 && probability !== 1) {
         probability = probability * (snapshotIndex) / (snapshots.length + bias) + 0.5 * bias / (snapshots.length + bias);
     }
+    let complementProbability = 1 - probability;
+
+    // To avoid the bars bumping out of the meter, discount their width
+    if (probability > 0.01) {
+        probability -= 0.01;
+    }
+    if (complementProbability > 0.01) {
+        complementProbability -= 0.01;
+    }
 
     let probString = getProbString(probability);
     prob.innerHTML = probString;
     whiteBar.style.width = probString;
-    redBar.style.width = getProbString(1 - probability);
+    redBar.style.width = getProbString(complementProbability);
 }
