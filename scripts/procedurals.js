8e425a02bb3ac79204ea2adbb6aed76ac1fc9356
Kantack, Nicholas R <nickkantack@gmail.com>
2023-08-19 17:43:33 -0400

    procedurals.js now renders the nav bar so it doesn't need to be duplicated across all relevant html files. Added About Me page but the content is a stub.

--- scripts/procedurals.js
+++ scripts/procedurals.js
@@ -4,30 +4,32 @@ function E(a){
 }
 //-------------------------------------------------------------------------------------------------------
 // Nav bar
-E("navHome").addEventListener("click", function(){ window.location.replace("./home.html"); });
-E("navResume").addEventListener("click", function(){
-	let a = document.createElement("a");
-	a.href="../pdf/resume.pdf";
-	a.download = "resume.pdf";
-	document.body.appendChild(a);
-	a.click();
-});
-E("navProjects").addEventListener("click", function(){ window.location.replace("./projects.html"); });
-E("navLiterature").addEventListener("click", function(){ window.location.replace("./literature.html"); });
-//-------------------------------------------------------------------------------------------------------
-// Nav bar stickiness
-/*
-var offset = E("navBar").offsetTop;
-window.onscroll = function() {
-  if (window.pageYOffset >= offset) {
-    E("navBar").classList.add("stickyNavBar")
-  } else {
-    E("navBar").classList.remove("stickyNavBar");
-  }
-};
-*/
-//-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------
+// The nav bar automation contract is that for each member of the list below, there exists a page
+// in pages that is the space-removed, camel-cased transformation of the name (unless it has 
+// special handling in the switch below).
+const navBarPages = ["Home", "Projects", "Literature", "About Me", "Resume"];
+function renderNavBar() {
+	const navBar = E("navBar");
+	for (let navBarPage of navBarPages) {
+		// Catch any special cases and handle them differently
+		const buttonId = `nav${navBarPage.replace(/\s/g, "")}`;
+		const button = document.createElement("button");
+		button.id = buttonId;
+		button.classList.add("navButton");
+		button.innerHTML = navBarPage;
+		navBar.appendChild(button);
+		switch (navBarPage) {
+			case "Resume":
+				button.addEventListener("click", () => {
+					let a = document.createElement("a");
+					a.href="../pdf/resume.pdf";
+					a.download = "resume.pdf";
+					document.body.appendChild(a);
+					a.click();
+				});
+			break;
+			default:
+				button.addEventListener("click", () => { window.location.href = `./${navBarPage.substring(0, 1).toLowerCase() + navBarPage.substring(1).replace(/\s/g, "")}.html`; });
+		}
+	}
+}
