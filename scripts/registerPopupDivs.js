709adf5aedaf11fc96cdac8747aa1101727f7949
Kantack, Nicholas R <nickkantack@gmail.com>
2023-09-02 10:14:27 -0400

    Lots of style improvements to the popup div added, and has a much more solid feel now

--- scripts/registerPopupDivs.js
+++ scripts/registerPopupDivs.js
@@ -1,13 +1,28 @@
+const POPUP_TRANSITION_TIME_MS = 300;
+// Check for any misconfiguration between CSS and this javascript, since we can't link the two
+if (document.getElementsByClassName("popupStoryDiv")) {
+    const expectedTransitionValueFromCss = `${POPUP_TRANSITION_TIME_MS / 1000}s`;
+    const foundTransitionValue = window.getComputedStyle(document.getElementsByClassName("popupStoryDiv")[0]).getPropertyValue("transition-duration");
+    if (foundTransitionValue !== expectedTransitionValueFromCss) {
+        console.warn("Check the 'transition' or 'transition-duration' property of the popupStoryDiv class. I expected that" +
+        ` 'transition-duration' would be ${expectedTransitionValueFromCss} but it was ${foundTransitionValue}.`);
+    }
+}
+console.log(document.getElementsByClassName("popupStoryDiv")[0].style);
 (function registerPopupStories() {
 	const popupStorySpans = document.querySelectorAll("span.popupStorySpan");
 	for (let popupStorySpan of popupStorySpans) {
 		const popupStoryDiv = document.getElementById(popupStorySpan.id.replace(/Span/, "Div"));
         function closePopupStoryDiv() {
+            // Actions to take immediately upon closing the popup
             popupStoryDiv.style.height = "0";
+            popupStoryDiv.style.overflowY = "hidden";
+            for (let p of popupStoryDiv.querySelectorAll("p")) p.style.opacity = "0";
             document.body.removeEventListener("click", closePopupStoryDivIfBody);
             setTimeout(() => {
+                // Actions to take once the popup has finished disappearing
                 popupStoryDiv.style.display = "none";
-            }, 500);
+            }, POPUP_TRANSITION_TIME_MS);
         };
         function closePopupStoryDivIfBody(e) {
             if (e.target.closest(".popupStoryDiv")) return;
@@ -19,15 +34,20 @@
 		}
         const closeButton = document.createElement("button");
         closeButton.innerHTML = "X";
-        closeButton.style = `cursor: pointer; width: 30px; height: 30px, border-radius: 5px; position: fixed; right: 0; top: 0`;
         popupStoryDiv.insertBefore(closeButton, popupStoryDiv.firstChild);
 		popupStorySpan.addEventListener("click", () => {
 			popupStoryDiv.style.display = "block";
             setTimeout(() => {
+                // Actions to take just as the popup has started appearing
                 popupStoryDiv.style.height = "400px";
                 closeButton.addEventListener("click", closePopupStoryDiv);
                 document.body.addEventListener("click", closePopupStoryDivIfBody);
             }, 10);
+            setTimeout(() => {
+                // Actions to take once the popup has finished appearing
+                popupStoryDiv.style.overflowY = "auto";
+                for (let p of popupStoryDiv.querySelectorAll("p")) p.style.opacity = "1";
+            }, POPUP_TRANSITION_TIME_MS);
 		});
         
 	}
