3f265914c0b4c4c7ebfacc3d0d5bd2ab67ded6a4
nickkantack <nickkantack@gmail.com>
2024-11-03 16:41:12 -0500

    Cosmetic changes and added some resources. Computer vision project page is in progress

--- scripts/procedurals.js
+++ scripts/procedurals.js
@@ -7,7 +7,7 @@ function E(a){
 // The nav bar automation contract is that for each member of the list below, there exists a page
 // in pages that is the space-removed, camel-cased transformation of the name (unless it has 
 // special handling in the switch below).
-const navBarPages = ["Home", "About Me", "Resume"];
+const navBarPages = ["Home", "Resume"];
 function renderNavBar() {
 	const navBar = E("navBar");
 	for (let navBarPage of navBarPages) {
